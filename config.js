0aa981e175187430a93b8228ab5e321d7516ce6c
oscarpitt2205 <78483850+oscarpitt2205@users.noreply.github.com>
2021-02-06 10:06:14 +0530

    Update and rename config.sample.js to config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+var config = {};
+
+config.telegram = {};
+config.telegram.apiKey = '1388528550:AAEpL25saj-euYRzVaF_PC1s6AyQYcm5JQg';
+config.telegram.chatId = '1001443200845';
+
+config.bitly = {};
+config.bitly.token = '77c6505bbd00dd169541e6c23e0468ed86160557';
+
+config.amazon = {};
+config.amazon.tag = '';
+config.amazon.country = '';
+
+config.server = {}
+config.server.url = '';
+
+module.exports = config;
